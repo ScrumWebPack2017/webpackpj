c88f415bd4b54a182759ee82f2df6c363d19e224
zharkovalexzander <oleksandr.zharkov@nure.ua>
2017-03-16 11:47:22 +0200

    I fixed PHPS

--- database_scripts/database_connect.php
+++ database_scripts/database_connect.php
@@ -5,13 +5,13 @@ $username = "root";
 $pass = "";
 $db_link = mysql_connect($server, $username, $pass);
 if(!$db_link) {
-    echo "Connection error: " . mysql_error();
+    echo "conn error";
     exit();
 }
 $db_selected = mysql_select_db("WebPackDB", $db_link);
 if (!$db_selected) {
-    echo "Error while selecting DB : " . mysql_error();
+    echo "error db";
     exit();
 }
-echo "OK\n";
+echo "OK";
 ?>
