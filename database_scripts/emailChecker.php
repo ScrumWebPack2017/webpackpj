734e0a211403f8259a7f2ec89076a93f04ef3d30
zharkovalexzander <oleksandr.zharkov@nure.ua>
2017-03-17 18:40:17 +0200

    Change email-checkin func, code formatted

--- database_scripts/emailChecker.php
+++ database_scripts/emailChecker.php
@@ -1,15 +1,19 @@
 <?php
-include "database_connect.php";
-require ("database_framework.php");
-if(!$_POST["email"]) {
-    echo "post fail";
-    exit();
-} else {
-    if (empty($_POST["email"])) {
-        echo "empty";
+    namespace WebPack;
+    require ("database_framework.php");
+    include "database_connect.php";
+    if(!$_POST["email"]) {
+        echo "post fail";
         exit();
+    } else {
+        if (empty($_POST["email"])) {
+            echo "empty";
+            exit();
+        }
     }
-}
-if(checkEmail($_POST["email"])) echo "yes";
-else echo "no";
+    if(checkForEmail($_POST["email"])){
+        echo "no";
+    } else {
+        echo "yes";
+    }
 ?>
